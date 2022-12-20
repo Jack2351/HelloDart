1e927da625a5fa00d5d3acd51dd8d3781c8b2283
justina0 <2264114895@qq.com>
2022-12-20 10:25:28 +0000

    changes

--- 04_core.dart
+++ 04_core.dart
@@ -2,7 +2,7 @@
 
 import 'dart:math';
 
-void main(){
+void main(List<String> args){
   var numbers = RegExp(r'\d+');
   var someDigits = 'llamas live 15 to 20 years';
   // Check whether the reg exp has a match in a string.
@@ -11,6 +11,12 @@ void main(){
   for (final match in numbers.allMatches(someDigits)) {
   print(match.group(0)); // 15, then 20
   }
-  
 
+  // Use split() with an empty string parameter to get
+  // a list of all characters (as Strings); good for
+  // iterating.
+  for (final char in 'hello'.split('')) {
+  print(char);
+  }
+  
 }
